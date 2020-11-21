9ff9308ba2102879a9426ba59a0b2417c72e1285
GowthamG455 <73981374+GowthamG455@users.noreply.github.com>
2020-11-21 17:40:31 +0530

    Add files via upload

---
+++ social media integration/precache-manifest.7cc5768b3a21121a2be73380983d3654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8703e002879027e4e97b97263e851d7a",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "4ce88a2f70c9b65e3ffc",
+    "url": "/login/static/css/main.62789ce1.chunk.css"
+  },
+  {
+    "revision": "d53834f314abb0b803b7",
+    "url": "/login/static/js/2.afc33b90.chunk.js"
+  },
+  {
+    "revision": "a1212b395e88f070a42368ea2ff93e06",
+    "url": "/login/static/js/2.afc33b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce88a2f70c9b65e3ffc",
+    "url": "/login/static/js/main.74aae43e.chunk.js"
+  },
+  {
+    "revision": "2a6ddebc308cbef80bfa",
+    "url": "/login/static/js/runtime-main.eced9b5c.js"
+  }
+]);
